49384e1f7c1c4a972c603bf50518697a688e4b9f
info206 <info206@LAB02-07.edupit.intra>
2015-06-01 14:46:23 -0300

    Revert "inicial"
    This reverts commit 7a98a098ee99d4dab96d5d88de7bdcc51be53146.

--- src/petshop/Petshop.java
+++ src/petshop/Petshop.java
@@ -4,8 +4,6 @@
  */
 package petshop;
 
-import java.util.Scanner;
-
 /**
  *
  * @author info206
@@ -16,15 +14,6 @@ public class Petshop {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int FATOR1 ,RESULTADO = 1;
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("digite o numero");
-        FATOR1  = teclado.nextInt();
-        
-        
-        for(int fat=FATOR1;FATOR1>1;FATOR1--){
-            RESULTADO= RESULTADO*FATOR1;
-                    System.out.println(" " +RESULTADO);
-        }
+        // TODO code application logic here
     }
 }
